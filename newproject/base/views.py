4e41151b8ed59304bd20608749d6817089e981af
Satyajeet <satyjeetsatyam5@gmail.com>
2025-09-10 02:18:49 +0530

    templatesa and dynamic routing

--- newproject/base/views.py
+++ newproject/base/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, World!")
 
-def room(request):
-    return HttpResponse("welcome to my room")
+rooms = [
+    {'id': 1, 'name': 'lets learn Django'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
+
+def home(request):
+    context = {'rooms' : rooms}
+    return render(request,'base/home.html', context)
+    # return render(request,'home.html', {'rooms': rooms})
+
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html',context)
